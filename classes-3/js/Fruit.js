2b41472ac49440a3695dd24a4d861ca150c9e358
Geoff Groenendale <ggroenendale@wsu.edu>
2020-02-01 06:38:27 -0800

    Final Updates

--- classes-3/js/Fruit.js
+++ classes-3/js/Fruit.js
@@ -1,14 +1,28 @@
+/**
+ * 
+ */
 class Fruit {
 
+    /**
+     * 
+     * @param {String} name 
+     * @param {Number} price 
+     */
     constructor(name, price) {
         this.price = price;
         this.name = name;
     }
 
+    /**
+     * 
+     */
     get price() {
         return this.price;
     }
 
+    /**
+     * 
+     */
     set price(val) {
         if (val <= 0 ){
             alert("Value must be greater than 0.");
@@ -17,18 +31,24 @@ class Fruit {
         this._price = val;
     }
 
+    /**
+     * 
+     */
     get name() {
         return this.name;
     }
 
+    /**
+     * 
+     */
     set name(input) {
-        if (input.match(/^[^a-zA-Z]+$/)) {
-            console.log('The test is true');
-        }
-        else {
-            console.log('The test is false');
+        // This test checks if the string contains anything other than letters
+        if (input.match(/[^a-zA-Z]/)) {
+            console.log('The provided string contains bad characters');
+            this._name = 'ERROR';
+            return;
         }
         
-        this._name = name;
+        this._name = input;
     }
 }
--- classes-3/js/scripts.js
+++ classes-3/js/scripts.js
@@ -1,3 +1,52 @@
 var gala = new Apple('Gala', 0.49, 'red');
 
+console.log(gala);
 
+
+function build_page(){
+    let sidebar = document.getElementById('sidebar');
+
+    let form = document.createElement('div');
+    form.style.padding = '2%';
+
+    let name = document.createElement('input');
+    name.type = 'text';
+    name.id = 'name_in';
+    name.name = 'name';
+    let name_label = document.createElement('label');
+    name_label.htmlFor = 'name_in';
+    name_label.innerText = 'Name: ';
+    
+    let price_in = document.createElement('input');
+    price_in.id = 'price_in';
+    price_in.type = 'text';
+    price_in.name = 'price';
+    let price_label = document.createElement('label');
+    price_label.htmlFor = 'price_in';
+    price_label.innerText = 'Price: ';
+    
+    let color_in = document.createElement('input');
+    color_in.id = 'color_in';
+    color_in.type = 'text';
+    color_in.name = 'color';
+    let color_label = document.createElement('label');
+    color_label.htmlFor = 'color_in';
+    color_label.innerText = 'Color: ';
+
+    let button = document.createElement('button');
+    button.innerText = 'Create Apple';
+    button.onclick = create_apple();
+
+    form.append(name_label, name, price_label, price_in, color_label, color_in, button);
+    sidebar.append(form);
+}
+
+function create_apple(){
+    
+}
+
+function main() {
+    build_page();
+}
+
+window.onload = main();
